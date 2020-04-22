ac010fe2380d6ceb3608023d9468cc3d2c7da28b
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2020-04-22 13:02:13 +0200

    #5825 Extract data from inactive regions for fracture model.

--- ApplicationCode/ReservoirDataModel/RigFractureModelLogExtractor.cpp
+++ ApplicationCode/ReservoirDataModel/RigFractureModelLogExtractor.cpp
@@ -35,6 +35,14 @@
 
 #include <iostream>
 #include <map>
+#include <sstream>
+
+std::string toString( const cvf::Vec3d& vec )
+{
+    std::stringstream stream;
+    stream << "[" << vec.x() << " " << vec.y() << " " << vec.z() << "]";
+    return stream.str();
+}
 
 //==================================================================================================
 ///
@@ -50,29 +58,46 @@ RigFractureModelLogExtractor::RigFractureModelLogExtractor( const RigEclipseCase
 
     m_wellPath = new RigWellPath;
 
-    const cvf::BoundingBox& geometryBoundingBox = aCase->activeCellInfo( RiaDefines::MATRIX_MODEL )->geometryBoundingBox();
+    const cvf::BoundingBox& geometryBoundingBox = aCase->mainGrid()->boundingBox();
 
-    // Position on top of formation
+    std::cout << "All cells bounding box: " << toString( geometryBoundingBox.min() ) << " "
+              << toString( geometryBoundingBox.max() ) << std::endl;
+    std::cout << "Position: " << toString( position ) << std::endl;
+    std::cout << "Direction: " << toString( direction ) << std::endl;
+
+    // Create plane on top and bottom of formation
     cvf::Plane topPlane;
     topPlane.setFromPointAndNormal( geometryBoundingBox.max(), cvf::Vec3d::Z_AXIS );
-    cvf::Vec3d abovePlane = position + ( direction * 10000.0 );
+    cvf::Plane bottomPlane;
+    bottomPlane.setFromPointAndNormal( geometryBoundingBox.min(), cvf::Vec3d::Z_AXIS );
+
+    // Convert direction up for z
+    cvf::Vec3d directionUp( direction.x(), direction.y(), -direction.z() );
+
+    // Find and add point on top plane
+    cvf::Vec3d abovePlane = position + ( directionUp * 10000.0 );
     cvf::Vec3d topPosition;
     topPlane.intersect( position, abovePlane, &topPosition );
-
-    double depthDiff = 0.0 - topPosition.z();
-    m_wellPath->setDatumElevation( topPosition.z() );
-
-    std::cout << "TOP POSITION: " << topPosition.z() << " MD: " << 0.0 << std::endl;
-
+    double topMD = 0.0;
     m_wellPath->m_wellPathPoints.push_back( topPosition );
-    m_wellPath->m_measuredDepths.push_back( 0.0 ); // topPosition.z() ); // geometryBoundingBox.max().z() );
-                                                   // // measuredDepth );
+    m_wellPath->m_measuredDepths.push_back( topMD );
+    std::cout << "TOP:    " << toString( topPosition ) << " MD: " << topMD << std::endl;
 
     // The anchor position
-    m_wellPath->m_wellPathPoints.push_back( position );
     double dist = ( topPosition - position ).length();
-    std::cout << "ANCHOR: " << position.z() << " MD: " << dist << std::endl;
-    m_wellPath->m_measuredDepths.push_back( dist ); // measuredDepth + depthDiff );
+    m_wellPath->m_wellPathPoints.push_back( position );
+    m_wellPath->m_measuredDepths.push_back( dist );
+    std::cout << "ANCHOR: " << toString( position ) << " MD: " << dist << std::endl;
+
+    // Find and add point on bottom plane
+    cvf::Vec3d belowPlane = position + ( directionUp * -10000.0 );
+    cvf::Vec3d bottomPosition;
+    bottomPlane.intersect( position, belowPlane, &bottomPosition );
+
+    double dist2 = ( topPosition - bottomPosition ).length();
+    m_wellPath->m_wellPathPoints.push_back( bottomPosition );
+    m_wellPath->m_measuredDepths.push_back( dist2 );
+    std::cout << "BOTTOM: " << toString( bottomPosition ) << " MD: " << dist2 << std::endl;
 
     m_wellLogExtractor = new RigEclipseWellLogExtractor( aCase, &*m_wellPath, "fracture model" );
 }
